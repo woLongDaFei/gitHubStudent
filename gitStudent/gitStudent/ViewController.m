2dd830a0578c9d61c6ccbd8ed3bb69e03d640e1c
woLongDaFei <zhaoyongfei2008@163.com>
2020-08-15 23:05:41 +0800

    提交git commit -m "Removed file that shouldn't be tracked"

--- gitStudent/gitStudent/ViewController.m
+++ gitStudent/gitStudent/ViewController.m
@@ -22,10 +22,11 @@
     [self.view addSubview:view];
     
 //    dev1
-    
     UIView *view1 = [[UIView alloc] init];
     [self.view addSubview:view1];
 }
 
+git rm --cached 工程的名字.xcodeproj/project.xcworkspace/xcuserdata/你的用户名.xcuserdatad/UserInterfaceState.xcuserstate
 
+git commit -m "Removed file that shouldn't be tracked"
 @end
