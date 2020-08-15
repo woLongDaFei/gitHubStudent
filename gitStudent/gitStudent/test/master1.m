3c3a1d90d392db30ddb2c0b2aedd4c7de23f3084
woLongDaFei <zhaoyongfei2008@163.com>
2020-08-15 22:49:32 +0800

    修改错误

--- gitStudent/gitStudent/test/master1.m
+++ gitStudent/gitStudent/test/master1.m
@@ -8,15 +8,10 @@
 
 #import "master1.h"
 
-<<<<<<< HEAD:gitStudent/gitStudent/test/dev1.m
-@implementation dev1
-- (void)dev1 {
-=======
 @implementation master1
 
 - (void)master11 {
     
->>>>>>> parent of bba6f99... 删除master1:gitStudent/gitStudent/test/master1.m
     
 }
 
